aec1d649ab63e7d2e175425c12d993b963e9789b
Jinhuyk <mjh7131@naver.com>
2023-02-25 19:27:59 +0900

    [수정] 채팅상대의 가명 같이 띄우기 기능 추가

---
+++ frontend/scripts/nickname.js
@@ -0,0 +1,4 @@
+var nickname = "unknown";
+window.onload = ()=>{
+    nickname = prompt("what is your name?");
+}
--- frontend/scripts/socket.js
+++ frontend/scripts/socket.js
@@ -1,12 +1,30 @@
 const socket = io();
-
 const chatForm = document.getElementById('chat-form');
 const chatBox = document.getElementById('messages');
 
+
+
+
+function OthersMessageForm(nickname,message){
+    var HTML = '<div>'+nickname+'</div><div class = "others-message-wrapper">'+message+"</div>";
+    
+    return HTML;
+}
+
+function NowTime(){
+    const date = new Date();
+
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+    const timeStr = hours + ':' + minutes + ':' + seconds;
+    return timeStr;
+}
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = e.target.m.value
-    socket.emit('chat message', message);
+    // nickname use
+    if(message) socket.emit('chat message', OthersMessageForm(nickname,message));
     e.target.m.value = '';
     if(message) chatBox.appendChild(makeMessage(message, false));
 })
@@ -17,8 +35,8 @@ socket.on('chat message', (message) => {
 
 const makeMessage = (message, isOthers) => {
     const msgBox = document.createElement('div');
-    const classname = isOthers ? "others-message-wrapper" : "my-message-wrapper";
+    const classname = isOthers ? "others-message" : "my-message-wrapper";
     msgBox.className = classname;
-    msgBox.innerText = message;
+    msgBox.innerHTML = message;
     return msgBox;
 }
